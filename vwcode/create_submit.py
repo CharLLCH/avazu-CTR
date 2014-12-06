2517182a5d19a32b6d4f41fa618efe29d7fdb7b3
CHarCH <charlley88@163.com>
2014-12-06 22:34:21 +0800

    test to create new kind pred

--- vwcode/create_submit.py
+++ vwcode/create_submit.py
@@ -5,9 +5,9 @@ import math
 def zymoid(x):
     return 1 / ( 1 + math.exp(-x) )
 
-with open('vw_submit.csv','wb') as outfile:
+with open('../result/vw_submit.csv','wb') as outfile:
     outfile.write("id,click\n")
-    for line in open('pred_vw.txt'):
+    for line in open('../result/pred_vw.txt'):
         row = line.strip().split(" ")
         try:
             outfile.write("%s,%f\n"%(row[1],zymoid(float(row[0]))))
